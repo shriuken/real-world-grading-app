874fd016b1c93da34241a7b92b6df4b059b93a0a
Ryan Benner <Ryan_Benner@intuit.com>
2020-10-22 17:12:33 -0700

    feat: seed user data

--- src/seed.ts
+++ src/seed.ts
@@ -4,7 +4,25 @@ import { add } from 'date-fns'
 const prisma = new PrismaClient()
 
 // A `main` function so that we can use async/await
-async function main() {}
+async function main() {
+  await prisma.testResult.deleteMany({})
+  await prisma.courseEnrollment.deleteMany({})
+  await prisma.test.deleteMany({})
+  await prisma.user.deleteMany({})
+  await prisma.course.deleteMany({})
+
+  const grace = await prisma.user.create({
+    data: {
+      email: 'grace@hey.com',
+      firstName: 'Grace',
+      lastName: 'Bell',
+      social: {
+        facebook: 'gracebell',
+        twitter: 'therealgracebell'
+      }
+    }
+  })
+}
 
 main()
   .catch((e: Error) => {
